488082dbaef5750793eadb194a063888ff77144d
Alex Friedman <a.friedman07@gmail.com>
2017-01-15 03:30:54 -0500

    tweaked webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     output: {
         filename: "bundle.js",
-        path: __dirname + "/public"
+        path: __dirname + "/backend/public"
     },
 
     // Enable sourcemaps for debugging webpack's output.
