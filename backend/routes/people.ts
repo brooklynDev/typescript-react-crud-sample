372711a451ed982560f424c1ba17532e2977abc3
Alex Friedman <a.friedman07@gmail.com>
2017-01-15 02:20:13 -0500

    Added safety check in edit route

--- backend/routes/people.ts
+++ backend/routes/people.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
-import {people} from '../db';
-import {isPerson} from '../models/Person';
+import { people } from '../db';
+import { isPerson } from '../models/Person';
 
 const router = express.Router();
 
@@ -10,20 +10,24 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    if(!isPerson(req.body)){
-        res.status(403).json({error: 'Invalid Person object'});
+    if (!isPerson(req.body)) {
+        res.status(403).json({ error: 'Invalid Person object' });
         return;
     }
     let result = await people.addPerson(req.body);
     res.json(result);
 });
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
     let result = await people.deletePerson(req.params.id);
     res.json(result);
 });
 
-router.put('/', async(req, res) => {
+router.put('/', async (req, res) => {
+    if (!isPerson(req.body)) {
+        res.status(403).json({ error: 'Invalid Person object' });
+        return;
+    }
     let result = await people.updatePerson(req.body);
     res.json(result);
 });
